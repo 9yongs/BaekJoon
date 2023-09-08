062901dd915d25eaa52883fb50f2bcfccb7e2366
9yongs <142721588+9yongs@users.noreply.github.com>
2023-09-08 14:21:23 +0900

    Update 윤년.java

--- 조건문/윤년.java
+++ 조건문/윤년.java
@@ -2,6 +2,9 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 
+// 2023.09.08 작성
+
+
 public class Main {
     public static void main(String[] args) throws IOException{
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
