70346284e798df89b95247bb31b31971b19e2ae9
9yongs <142721588+9yongs@users.noreply.github.com>
2023-09-19 16:01:14 +0900

    2562번 : 최댓값

---
+++ 1차원 배열/2562.java
@@ -0,0 +1,28 @@
+/* 23.09.19 작성 */
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int [] arr = new int[9];
+
+        int max = -101;
+        int count = 0;
+
+        for (int i = 0; i < arr.length; i++) {
+            int N = Integer.parseInt(br.readLine());
+            if (max < N) {
+                max = N;
+                count = i+1;
+            }
+        }
+        System.out.println(max);
+        System.out.println(count);
+    }
+}
