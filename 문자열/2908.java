7c104d0d634e47e38b19f3441c8513cabb03d53c
9yongs <142721588+9yongs@users.noreply.github.com>
2023-09-25 16:13:14 +0900

    2908번 : 상수

--- 문자열/2908.java
+++ 문자열/2908.java
@@ -11,8 +11,8 @@ public class Main {
 		
 		StringTokenizer st = new StringTokenizer(br.readLine()," ");
 
-		int A = Integer.parseInt(new StringBuilder(st.nextToken()).reverse().toString());
-		int B = Integer.parseInt(new StringBuilder(st.nextToken()).reverse().toString());
+		int A = Integer.parseInt(new StringBuilder(st.nextToken()).reverse().toString()); // StringBuilder 에 있는 문자열을 뒤집는 메소드 reverse()
+		int B = Integer.parseInt(new StringBuilder(st.nextToken()).reverse().toString()); // 뒤집은 문자열을 Integer.parseInt로 int값으로 변경
 
 		System.out.println(A>B ? A:B); // (조건식 ? 참 값 : 거짓 값)
 	}
