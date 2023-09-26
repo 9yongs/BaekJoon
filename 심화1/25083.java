2d3a3ba7617f3146c3fd96e687a71e23f34e8329
9yongs <142721588+9yongs@users.noreply.github.com>
2023-09-26 14:16:15 +0900

    25083번 : 새싹

---
+++ 심화1/25083.java
@@ -0,0 +1,33 @@
+/* 23.09.26 작성 */
+// 문자열 출력
+
+public class Main {
+	public static void main(String[] args){
+		String s = "         ,r\'\"7\n" +
+					"r`-_   ,\'  ,/\n" +
+					" \\. \". L_r'\n" + 
+					"   `~\\/\n"+
+					"      |\n" +
+					"      |" ;
+		
+		System.out.println(s);
+	}
+}
+
+
+..
+/* 23.09.26 작성 */
+// 따로 따로 출력
+
+public class Main {
+	public static void main(String[] args){
+
+		System.out.println("         ,r\'\"7");
+		System.out.println("r`-_   ,\'  ,/");
+		System.out.println(" \\. \". L_r'");
+		System.out.println("   `~\\/");
+		System.out.println("      |");
+		System.out.println("      |");
+		
+	}
+}
